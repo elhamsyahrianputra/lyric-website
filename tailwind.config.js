60511107c54756b0a2902c739ae7768268381cbd
elhamsyahrianputra <72007074+elhamsyahrianputra@users.noreply.github.com>
2023-03-20 03:29:59 +0700

    feat: custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,43 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        negative: {
+          light: '#f47961',
+          DEFAULT: '#ea4f30',
+          dark: '#d72f0f',
+        },
+
+        positive: {
+          light: '#35df90',
+          DEFAULT: '#1eb971',
+          dark: '#188f57',
+        },
+
+        warning: {
+          light: '#fac661',
+          DEFAULT: '#f1ae2d',
+          dark: '#e49807',
+        },
+
+        accent: {
+          light: '#33a9ff',
+          DEFAULT: '#0090f7',
+          dark: '#0071c2',
+        },
+
+        lake: {
+          100: '#2f4765',
+          200: '#2a405b',
+          300: '#253850',
+          400: '#203145',
+          500: '#1c2a3b',
+          600: '#172331',
+          700: '#131e2a',
+        },
+      },
+    },
   },
   plugins: [],
 }
